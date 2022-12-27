3c020f5d11e70a2ebfd816191353671af8368bf6
j-w-park <okml133@gmail.com>
2022-12-28 00:43:10 +0900

    add launch.json

---
+++ index.ts
@@ -0,0 +1,27 @@
+function* generator() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+function* generator2() {
+  yield* generator();
+  yield 4;
+  yield 5;
+}
+
+function* generator3() {
+  yield* generator2();
+  yield 6;
+  yield 7;
+}
+
+const gen = generator3();
+console.log(gen.next(10));
+console.log(gen.next());
+console.log(gen.return());
+
+// while(gen.) {
+// }
+
+// console.log([...generator3()]);
