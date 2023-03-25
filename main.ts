7010e169227942f0eee93d3e722a5b86c6ada4b6
j-w-park <okml133@gmail.com>
2023-03-26 01:34:29 +0900

    rename

--- main.ts
+++
@@ -1,7 +0,0 @@
-import { Person } from "./src/decorators/person.js";
-import { example5 } from "./src/event-loop/index.js";
-
-const p = new Person("Ron");
-p.greet();
-
-example5();
---
+++ src/decorators/main.ts
@@ -0,0 +1,6 @@
+import { Person } from './person.js';
+
+export default function main() {
+  const p = new Person('Ron');
+  p.greet();
+}
